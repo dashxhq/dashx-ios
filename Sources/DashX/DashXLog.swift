a4dfe8a69c1aad83a027b17bffa6f3871fe2735e
Pradeep Kumar <pradeep@keepworks.com>
2022-12-26 21:25:01 +0530

    Make DashXLog class public

--- Sources/DashX/DashXLog.swift
+++ Sources/DashX/DashXLog.swift
@@ -1,43 +1,50 @@
 import Foundation
 import os.log
 
-class DashXLog {
-    enum LogLevel: Int {
+public class DashXLog {
+    public enum LogLevel: Int {
+        case debug = 2
         case info = 1
-        case debug = 0
+        case error = 0
         case off = -1
 
-        static func <= (lhs: Self, rhs: Self) -> Bool {
-            return lhs.rawValue <= rhs.rawValue
-        }
-
-        func on () -> Bool {
-            return self.rawValue > LogLevel.off.rawValue
+        static func >= (lhs: Self, rhs: Self) -> Bool {
+            return lhs.rawValue >= rhs.rawValue
         }
     }
 
     static private var logLevel: LogLevel = .off
 
-    static func setLogLevel(to: Int) {
-        self.logLevel = LogLevel(rawValue: to) ?? .off
+    static public func setLogLevel(to: LogLevel) {
+        self.logLevel = to
     }
 
     static func d(tag: String, _ data: String) {
-        if logLevel.on() && logLevel <= .debug {
+        if logLevel >= .debug {
             if #available(iOS 10.0, *) {
                 os_log("%@: %@", type: .debug, tag, data)
             } else {
-                print("%@: %@", tag, data)
+                print("[DEBUG] %@: %@", tag, data)
             }
         }
     }
 
     static func i(tag: String, _ data: String) {
-        if logLevel.on() && logLevel <= .info {
+        if logLevel >= .info {
             if #available(iOS 10.0, *) {
                 os_log("%@: %@", type: .info, tag, data)
             } else {
-                print("%@: %@", tag, data)
+                print("[INFO] %@: %@", tag, data)
+            }
+        }
+    }
+
+    static func e(tag: String, _ data: String) {
+        if logLevel >= .error {
+            if #available(iOS 10.0, *) {
+                os_log("%@: %@", type: .error, tag, data)
+            } else {
+                print("[ERROR] %@: %@", tag, data)
             }
         }
     }
