897f6a4dd63d1b3cabc4b7c5fa3e8e84fbbc952b
Amandeep <amandeep@keepworks.com>
2023-08-14 22:41:29 +0530

    Bump version to 1.0.20

--- DashX.podspec
+++ DashX.podspec
@@ -17,7 +17,7 @@ Pod::Spec.new do |spec|
 
   spec.name         = "DashX"
   spec.module_name  = "DashX"
-  spec.version      = "1.0.19"
+  spec.version      = "1.0.20"
   spec.summary      = "DashX SDK for iOS"
 
   spec.description  = <<-DESC
--- Sources/DashX/Constants.swift
+++ Sources/DashX/Constants.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public struct Constants {
     static let PACKAGE_NAME = "com.dashx.sdk"
-    static let PACKAGE_VERSION = "1.0.19"
+    static let PACKAGE_VERSION = "1.0.20"
     public static let USER_PREFERENCES_KEY_ACCOUNT_UID = "\(PACKAGE_NAME).account_uid"
     public static let USER_PREFERENCES_KEY_ACCOUNT_ANONYMOUS_UID = "\(PACKAGE_NAME).account_anonymous_uid"
     public static let USER_PREFERENCES_KEY_IDENTITY_TOKEN = "\(PACKAGE_NAME).identity_token"
